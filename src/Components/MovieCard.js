65ff6921c7bd28edcdceff51501e965e20df7e03
Anand Verma <anandverma552@gmail.com>
2024-03-17 23:21:08 -0400

    css fixed

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -4,7 +4,7 @@ import { IMG_CDN } from "../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return;
   return (
-    <div className="w-48 p-2">
+    <div className="w-48 p-2 hover:scale-105 hover:py-0 hover:w-60 py-6">
       <img alt="Movie card" src={IMG_CDN + posterPath}></img>
     </div>
   );
